d0f6f70200a1bded468fe3ab5c5f0c1bd963249e
lwil39 <63265110+lwil39@users.noreply.github.com>
2020-04-08 10:35:18 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
       ensure => present,
       user => 'root',
       type => 'ssh-rsa',
-      key => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4LzIMx2G/jA7+qA66TS1CjYyH9t1xAWZbxwchCRKguB+8y6xWjX6v22fl1BGR+qqUR1yS1nS1HKBDpwtunntjmpGswEbwmnQvsPh5YS+yUV0UkhRLdHjW+s65fWlbROQ80n8+RDni3LGvd8gJQ17NggpzsBlpaS2W/XN1JpbyCIAqsNvzPhyJri1silGe5nAuAlObX3+nZzBcF5T6Z+uFbZxMqmhKg0VetFpJOj/S6kzulkAZoUDEa1wStuuUfnW9pz8TZFTVTlXv7so42pfbPPgtcEKsWBJRnpBYbFqGFea133lsefXbUSlQtgBoOfZwFb2VaJ7e2yJPZef+rWa7',
+      key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC4LzIMx2G/jA7+qA66TS1CjYyH9t1xAWZbxwchCRKguB+8y6xWjX6v22fl1BGR+qqUR1yS1nS1HKBDpwtunntjmpGswEbwmnQvsPh5YS+yUV0UkhRLdHjW+s65fWlbROQ80n8+RDni3LGvd8gJQ17NggpzsBlpaS2W/XN1JpbyCIAqsNvzPhyJri1silGe5nAuAlObX3+nZzBcF5T6Z+uFbZxMqmhKg0VetFpJOj/S6kzulkAZoUDEa1wStuuUfnW9pz8TZFTVTlXv7so42pfbPPgtcEKsWBJRnpBYbFqGFea133lsefXbUSlQtgBoOfZwFb2VaJ7e2yJPZef+rWa7',
     }
 }
