aff40be47d123d2086c39bd604d88c4e1212d4ae
lwil39 <63265110+lwil39@users.noreply.github.com>
2020-05-04 13:30:59 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,14 +7,14 @@ class minecraft {
     source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
   package {'java':
-    ensure => present
+    ensure => present,
   }
   file {'/opt/minecraft/eula.txt':
-    ensure => file
+    ensure => file,
     content => 'eula=true',
   }
   file {'/etc/systemd/system/minecraft.service':
-    ensure => file
+    ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
   service {'minecraft':
